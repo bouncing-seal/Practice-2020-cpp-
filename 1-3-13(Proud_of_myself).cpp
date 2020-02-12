f27a6d890d77785ebb55728efdfc5ecdd68b04d4
Ivan Kurapov <54911787+bouncing-seal@users.noreply.github.com>
2020-02-13 00:52:00 +0300

    Create 1-3-13(Proud_of_myself).cpp

---
+++ 1-3-13(Proud_of_myself).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int H, A, B;
+  cin >> H, A, B;
+  cout << (H + B + A);
+  //cout << ((H - A) + (A - B) - 1) / (A - B) + 1;
+  return 0;
+}
